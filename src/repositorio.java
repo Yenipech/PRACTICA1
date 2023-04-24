17c2e4f4f4eb9259da4248f620eea4f32ea29094
Yeni <yenip@LAPTOP-UCCBN326>
2023-04-24 09:17:56 -0500

    Cambio0

---
+++ src/repositorio.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class repositorio {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nombre?");
+		String nombre = sc.next();
+		
+		System.out.println("Tu nombre es: " + nombre);
+	
+	}
+}
